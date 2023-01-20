705e4405e38709cfcfe601a3dba4d0080a27b7a1
mohammad <nasrmohammad4804@gmail.com>
2023-01-20 19:43:21 +0330

    create cqrs with event sourcing project based on axon framework for event store

---
+++ cqrs/cqrs-event-sourcing-axon/src/main/java/com/nasr/cqrseventsourcingaxon/CqrsEventSourcingAxonApplication.java
@@ -0,0 +1,13 @@
+package com.nasr.cqrseventsourcingaxon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsEventSourcingAxonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqrsEventSourcingAxonApplication.class, args);
+    }
+
+}
