133bc4bcf49ade1aa6ecba517881d726ea98f2ab
mohammad <nasrmohammad4804@gmail.com>
2023-02-09 22:17:26 +0330

    add paymentDetail to PaymentProcessCancelledEvent for deposit to user account with this details

--- core/src/main/java/com/nasr/core/event/PaymentProcessCancelledEvent.java
+++ core/src/main/java/com/nasr/core/event/PaymentProcessCancelledEvent.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 
 import java.math.BigDecimal;
 
-import static com.nasr.core.enumeration.PaymentStatus.*;
+import static com.nasr.core.enumeration.PaymentStatus.FAILED;
+import static com.nasr.core.model.UserPaymentDetail.PaymentDetail;
 
 @Getter
 public class PaymentProcessCancelledEvent extends BaseEvent<String> {
@@ -17,15 +18,17 @@ public class PaymentProcessCancelledEvent extends BaseEvent<String> {
     private final BigDecimal totalAmount;
     private final PaymentStatus paymentStatus;
     private final OrderDetailData orderDetailData;
+    private final PaymentDetail paymentDetail;
     private final String reason;
 
     @Builder
-    public PaymentProcessCancelledEvent(String paymentId, String orderId,BigDecimal totalAmount,OrderDetailData orderDetailData, String reason) {
+    public PaymentProcessCancelledEvent(String paymentId, String orderId,BigDecimal totalAmount,OrderDetailData orderDetailData,PaymentDetail paymentDetail, String reason) {
         super(paymentId);
         this.orderId = orderId;
         this.totalAmount=totalAmount;
         this.paymentStatus = FAILED;
         this.orderDetailData=orderDetailData;
+        this.paymentDetail=paymentDetail;
         this.reason = reason;
     }
 }
