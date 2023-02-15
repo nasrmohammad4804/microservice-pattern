bee3999ce8d2d4bf25efc2edaa6357cd7bf1a2b3
mohammad <nasrmohammad4804@gmail.com>
2023-02-16 01:20:33 +0330

    add open api dependency for payment service

--- product-service/src/main/java/com/nasr/productservice/core/constant/ConstantField.java
+++ product-service/src/main/java/com/nasr/productservice/core/constant/ConstantField.java
@@ -3,6 +3,6 @@ package com.nasr.productservice.core.constant;
 public class ConstantField {
 
     public static final String PRODUCT_PROCESSING_GROUP="product-group";
-    public static final Integer PRODUCT_SNAPSHOT_THRESHOLD=3;
+    public static final Integer PRODUCT_SNAPSHOT_THRESHOLD=10;
     public static final String SNAPSHOT_TRIGGER_DEFINITION_BEAN_NAME="snapShotTriggerDefinition";
 }
